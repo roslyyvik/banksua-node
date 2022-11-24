0fcddc8c5a3c80342bb953c5c17b715003d4aaae
roslyyvik <53823670+roslyyvik@users.noreply.github.com>
2022-11-24 21:44:12 +0200

    Add files via upload

--- utils/index.js
+++ utils/index.js
@@ -1,1 +1,17 @@
+const { 
+  createJWT,
+  isTokenValid,
+  attachCookiesToResponse, 
+} = require('./jwt')
+const createTokenUser = require('./createTokenUser')
+const createHash = require('./createHash')
+const checkPermissions = require('./checkPermissions')
 
+module.exports = {
+  createJWT,
+  isTokenValid,
+  attachCookiesToResponse,
+  createTokenUser,
+  createHash,
+  checkPermissions,
+}
